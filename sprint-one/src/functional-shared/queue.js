a7a2f08c89b59d00efded3903c8dcaf5b7bfb95d
Kim Marquardt <kim.marquardt@gmail.com>
2014-07-23 16:29:10 -0700

    Implement functional-shared stack & queue.

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,9 +1,31 @@
 var makeQueue = function(){
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var q = {queueSize:0, head:0, tail:0, storage:{}};
+
+  q.enqueue = queueMethods.enqueue;
+  q.dequeue = queueMethods.dequeue;
+  q.size = queueMethods.size;
+
+  return q;
 };
 
-var queueMethods = {};
+var queueMethods = {
+    enqueue: function(value) {
+        this.storage[this.tail] = value;
+        this.queueSize++;
+        this.tail++;
+    },
+    dequeue: function() {
+        var rv = this.storage[this.head];
+        this.queueSize = (this.queueSize > 0) ? this.queueSize -= 1 : 0;
+        this.head++;
+        return rv;
+    },
+    size: function() {
+        return this.queueSize;
+    }
+};
 
 
 
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -3,31 +3,32 @@ var makeStack = function() {
   // but try not not reference your old code in writing the new style.
   var someInstance = {};
 
-  var storage = {};
-  var size = 0;
+  someInstance.storage = {};
+  someInstance.stackSize = 0;
 
   someInstance.push = stackMethods.push;
   someInstance.pop = stackMethods.pop;
   someInstance.size = stackMethods.size;
+
+  return someInstance;
 };
 
 var stackMethods = {
   push: function(value){
-      storage[this.size] = value;
-      this.size++;
+      this.storage[this.stackSize] = value;
+      this.stackSize++;
   },
 
   pop: function(){
-    if (this.size > 0){
-      var temp = storage[this.size - 1];
-      delete storage[this.size - 1];
-      size--;
+    if (this.stackSize > 0){
+      var temp = this.storage[this.stackSize - 1];
+      this.stackSize--;
     }
     return temp;
   },
 
   size: function(){
-    return this.size;
+    return this.stackSize;
   }
 };
 
